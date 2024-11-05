a649a3d102f0a7a3288ad0ae131b3c383517ab23
SantiTN77 <danitafur77@gmail.com>
2024-11-05 12:31:14 -0500

    Actualizaci├│n autom├ítica

--- controllers/inventario.php
+++ controllers/inventario.php
@@ -153,7 +153,37 @@ include_once('..//controllers/functions.php');
         </div>
     </div>
 
-    <script src="../public/js/script_inventario.js"></script>
+    <script src="../public/js/script.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const sidebar = document.querySelector(".sidebar");
+            const sidebarToggle = document.querySelector(".sidebarBtn");
+            const homeSection = document.querySelector(".home-section");
+
+            function toggleSidebar() {
+                sidebar.classList.toggle("close");
+                homeSection.classList.toggle("close");
+                sidebarToggle.classList.toggle("rotate");
+            }
+
+            if (sidebarToggle) {
+                sidebarToggle.addEventListener("click", toggleSidebar);
+            }
+
+            function handleResize() {
+                if (window.innerWidth <= 768) {
+                    sidebar.classList.add("close");
+                    homeSection.classList.add("close");
+                } else {
+                    sidebar.classList.remove("close");
+                    homeSection.classList.remove("close");
+                }
+            }
+
+            window.addEventListener('resize', handleResize);
+            handleResize(); // Llamar la función al cargar la página
+        });
+    </script>
 
 </body>
 </html>
